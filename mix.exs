10c358f25d01f9bd217c5b0739afe60dad78f8ac
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-30 16:22:33 -0500

    Adds absinthe to project

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule Perimeter.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe_ecto, "~> 0.1.0"},
+      {:absinthe_plug, "~> 1.3.0"}
     ]
   end
 
