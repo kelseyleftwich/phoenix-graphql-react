c12d6eff004ec485869b651706fc1c0e498a617b
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-30 16:47:56 -0500

    Adds seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Perimeter.Trails.Trailhead
+alias Perimeter.Repo
+
+%Trailhead{name: "University Gates (East)", latitude: 35.209971, longitude: -85.898189} |> Repo.insert!
+%Trailhead{name: "Green's View", latitude: 35.217451, longitude: -85.918485,} |> Repo.insert!
