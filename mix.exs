21feb1e92966fc831e0ed26981af9993d55fc80b
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-30 17:13:37 -0500

    Fixes dep versions

--- mix.exs
+++ mix.exs
@@ -43,8 +43,9 @@ defmodule Perimeter.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe_ecto, "~> 0.1.0"},
-      {:absinthe_plug, "~> 1.3.0"}
+      {:absinthe, "~> 1.4"},
+      {:absinthe_ecto, "~> 0.1.3"},
+      {:absinthe_plug, "~> 1.4"}
     ]
   end
 
