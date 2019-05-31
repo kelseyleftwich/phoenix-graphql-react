c5a06713cc085910e971c7543423ac19ac942f91
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-31 15:54:55 -0500

    Adds trails to seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,10 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Perimeter.Trails.Trailhead
+alias Perimeter.Trails.Trail
 alias Perimeter.Repo
 
 %Trailhead{name: "University Gates (East)", latitude: 35.209971, longitude: -85.898189} |> Repo.insert!
 %Trailhead{name: "Green's View", latitude: 35.217451, longitude: -85.918485,} |> Repo.insert!
+%Trail{name: "Morgan's Steep to the Memorial Cross", length: 1.0, foot_travel_only: true} |> Repo.insert!
+%Trail{name: "Northwest Section", length: 8.5, foot_travel_only: true} |> Repo.insert!
