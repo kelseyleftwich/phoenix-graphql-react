443045afbe70b7ca5688f8197cf37e7fb7e274fe
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-31 16:31:30 -0500

    Adds seeds data including associations

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,7 +14,12 @@ alias Perimeter.Trails.Trailhead
 alias Perimeter.Trails.Trail
 alias Perimeter.Repo
 
-%Trailhead{name: "University Gates (East)", latitude: 35.209971, longitude: -85.898189} |> Repo.insert!
-%Trailhead{name: "Green's View", latitude: 35.217451, longitude: -85.918485,} |> Repo.insert!
+university_gates_east_trailhead = %Trailhead{name: "University Gates (East)", latitude: 35.209971, longitude: -85.898189} |> Repo.insert!
+greens_view_trailhead = %Trailhead{name: "Green's View", latitude: 35.217451, longitude: -85.918485,} |> Repo.insert!
+shakerag_hollow_trail = %Trail{name: "Shakerag Hollow", length: 1.6, foot_travel_only: true, trailheads: [
+  university_gates_east_trailhead,
+  greens_view_trailhead
+]} |> Repo.insert!
 %Trail{name: "Morgan's Steep to the Memorial Cross", length: 1.0, foot_travel_only: true} |> Repo.insert!
 %Trail{name: "Northwest Section", length: 8.5, foot_travel_only: true} |> Repo.insert!
+
