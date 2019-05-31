def13917e7caccb9e8c01e1fd9a0ce1369e9c1f2
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-31 14:59:55 -0500

    Adds list that's populated w graphql query results

---
+++ react/perimeter/src/components/TrailsList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Trail from "./Trail";
+
+const TrailsList = () => (
+  <Query
+    query={gql`
+      {
+        trails {
+          name
+          length
+          footTravelOnly
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Good things take time....</p>;
+      if (error) return <p>Something went wrong...</p>;
+
+      return (
+        <div className="row">
+          {data.trails.map(trail => (
+            <Trail key={trail.name} trail={trail} />
+          ))}
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default TrailsList;
