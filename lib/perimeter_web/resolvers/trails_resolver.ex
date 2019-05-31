f9710e42207d1d21af11a0725da0f982bf185e2b
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-05-31 08:47:48 -0500

    Adds create trailhead mutation

--- lib/perimeter_web/resolvers/trails_resolver.ex
+++ lib/perimeter_web/resolvers/trails_resolver.ex
@@ -5,4 +5,21 @@ defmodule PerimeterWeb.TrailsResolver do
     trailheads = Trails.list_trailheads()
     {:ok, trailheads}
   end
+
+  def find(%{name: name}, _info) do
+    case Trails.get_trailhead_by_name(name) do
+      nil -> {:error, "Trailhead #{name} not found!"}
+      trailhead -> {:ok, trailhead}
+    end
+  end
+
+  def create_trailhead(_root, args, _info) do
+    # TODO: add detailed error message handling later
+    case Trails.create_trailhead(args) do
+      {:ok, trailhead} ->
+        {:ok, trailhead}
+      _error ->
+        {:error, "could not create trailhead"}
+    end
+  end
 end
--- lib/perimeter_web/schema.ex
+++ lib/perimeter_web/schema.ex
@@ -14,4 +14,15 @@ defmodule PerimeterWeb.Schema do
       resolve(&TrailsResolver.all_trailheads/3)
     end
   end
+
+  mutation do
+    field :create_trailhead, :trailhead do
+      arg :name, non_null(:string)
+      arg :latitude, non_null(:float)
+      arg :longitude, non_null(:float)
+  
+      resolve &TrailsResolver.create_trailhead/3
+    end
+  end
+
 end
